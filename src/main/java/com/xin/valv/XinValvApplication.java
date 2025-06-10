3e951e91fbdfd6a28e62c66ff2114e2c90b26809
xin <1246814044@qq.com>
2025-06-10 23:26:03 +0800

    项目初始化

---
+++ src/main/java/com/xin/valv/XinValvApplication.java
@@ -0,0 +1,13 @@
+package com.xin.valv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XinValvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XinValvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xin/valv/XinValvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xin.valv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XinValvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
